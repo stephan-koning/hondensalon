c57dda80aaaf540e935ba98d4b7ecc146efa7240
Stephan Koning <99175655+stephan-koning@users.noreply.github.com>
2024-12-25 01:58:20 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,7 +49,7 @@ export default defineNuxtConfig({
     },
   },
   site: {
-    url: "http://stephan-koning-hondensalon.nuxt.space/",
+    url: "https://trimfindr.netlify.app",
   },
   compatibilityDate: "2024-09-20",
 });
